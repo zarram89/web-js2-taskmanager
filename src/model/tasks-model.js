33d567d18c26252c4a7e16953fd5e74fc2329157
Ramil <zarram89@gmail.com>
2026-02-02 10:51:19 +0500

    4.1.4 Добавит классам приватные поля и геттеры
    Сделаем часть полей в классах приватными

--- src/model/tasks-model.js
+++ src/model/tasks-model.js
@@ -3,10 +3,10 @@ import {getRandomTask} from '../mock/task.js';
 const TASK_COUNT = 4;
 
 export default class TaskModel {
-  tasks = Array.from({length: TASK_COUNT}, getRandomTask);
+  #tasks = Array.from({length: TASK_COUNT}, getRandomTask);
 
-  getTasks() {
-    return this.tasks;
+  get tasks() {
+    return this.#tasks;
   }
 }
 
